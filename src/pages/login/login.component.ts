433800ce5aa38b8a3b6d7c30f1fb9e236c5282f2
Laxmikant Dange <dange.laxmiknat@gmail.com>
2017-12-29 09:43:12 +0530

    Added cordova firebase, Added firebase, angularfire2

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Firebase } from '@ionic-native/firebase';
 
 @Component({
   selector: 'aa-login',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['/login.component.scss']
 })
 export class LoginComponent {
+  constructor(private firebase: Firebase) {
 
+  }
 }
